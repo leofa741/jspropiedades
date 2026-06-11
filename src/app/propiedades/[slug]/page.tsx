161ada2cd5cc166161cb35d929587be993218c7d
Leonardo Arena <leonardofarena@gmail.com>
2026-06-10 21:57:29 -0300

    vn

--- src/app/propiedades/[slug]/page.tsx
+++ src/app/propiedades/[slug]/page.tsx
@@ -281,7 +281,7 @@ function PageContent() {
 
     if (!propiedad?.slug) return;
     const url = `${window.location.origin}/propiedades/${propiedad.slug}`;
-   // const text = `Mirá  propiedad: ${propiedad.titulo} - ${formatPrice(propiedad.precio.monto, propiedad.precio.moneda, propiedad.precio.tipo)}`;
+   // const text = `M propiedad: ${propiedad.titulo} - ${formatPrice(propiedad.precio.monto, propiedad.precio.moneda, propiedad.precio.tipo)}`;
 
     if (navigator.share) {
       try {
